05384363ed59fc665c78ee8993322df9e3d93b99
Edilma Riano <edilmaria@hotmail.com>
2023-05-28 09:20:41 -0400

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeoFriend.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var name = turnContext.Activity.Text;
+            var f = System.IO.Path.Combine(System.Environment.CurrentDirectory,@"WorldCities.csv");
+            var cd = new CountryData(f);
+            var cap = cd.GetCapital(name);
+            var replyText = cap == null 
+                ? $"Sorry, I don't know the capital of {name}." 
+                : $"The capital of {name} is {cap}!.";
+            await turnContext.SendActivityAsync(replyText);
+
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello There, my name is LilyBot.  I am your geography TA. Enter the name of a country/region and I will tell you its capital city ";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ CountryData.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace GeoFriend
+{
+    public class CountryData
+    {
+        protected Country[] Countries { get; set; }
+
+        public CountryData(string fn)
+        {
+            var data = System.IO.File.ReadAllLines(fn);
+            Countries = (from z in data.Skip(1)
+                         select new Country(z)).ToArray();
+        }
+        public string GetCapital(string country)
+        {
+          return Countries.FirstOrDefault(
+              c=> c.Name.ToLower()== country.ToLower())?.Capital;
+        }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public Country(string s)
+        {
+            var t = s.Split(',');
+            Capital = t[0].Trim('"');
+            Name = t[4].Trim('"');
+            float res;
+            Population = float.TryParse(t[9].Trim('"'), out res) ? (int)res : 0;
+        }
+    }
+
+
+
+
+}
+        
